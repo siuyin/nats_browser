c05575f4dc31f7d9541560e0afa7a9b2a22bd05e
Loh Siu Yin <siuyin@beyondbroadcast.com>
2026-01-29 13:55:35 +0800

    refactor: use shared nats connection

--- 02_backend/main.go
+++ 02_backend/main.go
@@ -10,25 +10,32 @@ import (
 	"github.com/siuyin/dflt"
 )
 
+var nc *nats.Conn
+
 func main() {
+	setupNATSConnection()
+	defer nc.Close()
+
 	http.Handle("/", http.FileServer(http.FS(public.Content)))
 	http.HandleFunc("/connect", connectToNATS)
 
 	port := dflt.EnvString("PORT", "8080")
 	url := dflt.EnvString("NATS_URL", "yourToken@yourNATSServer.com")
 	log.Printf("running web server: PORT=%s, NATS_URL=%s...", port, url[:9])
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Println(err)
+		return
+	}
+}
+func setupNATSConnection() {
+	var err error
+	url := dflt.EnvString("NATS_URL", "yourToken@yourNATSServer.com")
+	nc, err = nats.Connect(url)
+	if err != nil {
+		log.Fatal("setupNATSConnection:", err)
+	}
 }
 
 func connectToNATS(w http.ResponseWriter, r *http.Request) {
-	url := dflt.EnvString("NATS_URL", "yourToken@yourNATSServer.com")
-	nc, err := nats.Connect(url)
-	if err != nil {
-		log.Println("connectToNATS:", err)
-		return
-	}
-
-	defer nc.Close()
-
 	fmt.Fprintf(w, "Connected to NATS servers: %v\n", nc.Servers())
 }
